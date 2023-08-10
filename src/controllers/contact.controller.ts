ef9ef73653f877512b5539d2315216b2743a021e
MahedialiBhatt <bhattmahediali92@gmail.com>
2023-08-10 21:42:48 +0530

    Added email and phone number length validations

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -10,6 +10,22 @@ const ContactController = {
       return writeResponse({ code: 400, message: "Bad Request" }, null, res);
     }
 
+    if (email && email.length > 25) {
+      return writeResponse(
+        { code: 400, message: "Email length can't be greater than 25" },
+        null,
+        res
+      );
+    }
+
+    if (phoneNumber && phoneNumber.length > 16) {
+      return writeResponse(
+        { code: 400, message: "Phone number length can't be greater than 16" },
+        null,
+        res
+      );
+    }
+
     const [contacts, err0] = await invoker(
       contactService.identify(email, phoneNumber)
     );
